409571f97e4c99c986aa5a1e802d84fd89fe1902
Clement-R <rondeau.clement@gmail.com>
2017-04-03 14:52:06 +0200

    Add new scene to test texture animation. Add texture animation on Vuforia image mapping.

---
+++ Assets/TextureAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureAnimation : MonoBehaviour {
+    public List<Texture2D> frames = new List<Texture2D>();
+    public int framesPerSecond = 10;
+
+    private float timer = 0.0f;
+
+    void Update() {
+        int index = (int) (Time.time * framesPerSecond);
+        index = index % frames.Count;
+        GetComponent<Renderer>().material.mainTexture = frames[index];
+
+        // timer = (timer + Time.deltaTime * framesPerSecond) % frames.Count;
+        // Debug.Log(timer);
+        // GetComponent<SpriteRenderer>().sprite = frames[(int) timer];
+    }
+}
