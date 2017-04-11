5be7476c3b1b14d881e18c87e6bed55d8bdd4451
Clement-R <rondeau.clement@gmail.com>
2017-04-11 15:23:31 +0200

    Rework test scene to include the new ImageTarget prefab and test its behaviour. Add Wwise event in the multiplayer scene for test purpose.

--- Assets/Scripts/ModelSwap.cs
+++ Assets/Scripts/ModelSwap.cs
@@ -9,32 +9,18 @@ public class ModelSwap : MonoBehaviour
 {
     private GameObject mDefaultModel;
     private GameObject mExtTrackedModel;
-    private GameObject mActiveModel = null;
     private TrackableSettings mTrackableSettings = null;
 
 	void Start () 
     {
         mDefaultModel = this.transform.FindChild("teapot").gameObject;
-        mExtTrackedModel = this.transform.FindChild("tower").gameObject;
-        mActiveModel = mDefaultModel;
-        mTrackableSettings = FindObjectOfType<TrackableSettings>();
     }
     
     void Update () 
     {
-        if (mTrackableSettings.IsExtendedTrackingEnabled() && (mActiveModel == mDefaultModel))
+        if ((mExtTrackedModel != null))
         {
-            // Switch 3D model to tower
-            mDefaultModel.SetActive(false);
-            mExtTrackedModel.SetActive(true);
-            mActiveModel = mExtTrackedModel;
-        }
-        else if (!mTrackableSettings.IsExtendedTrackingEnabled() && (mActiveModel == mExtTrackedModel))
-        {
-            // Switch 3D model to teapot
-            mExtTrackedModel.SetActive(false);
             mDefaultModel.SetActive(true);
-            mActiveModel = mDefaultModel;
         }
     }
 }
