9ad656be617649b2ca426c869c9b2c89057760f4
Clement-R <rondeau.clement@gmail.com>
2017-04-20 17:54:00 +0200

    Add new albums

--- Assets/WwiseNetwork.cs
+++ Assets/WwiseNetwork.cs
@@ -10,12 +10,18 @@ public class WwiseNetwork : NetworkBehaviour {
     [Command]
     void CmdPlaySound(string eventName) {
         Debug.LogError("Hello world");
+        AkSoundEngine.PostEvent("congrats", gameObject);
     }
 
     void Update() {
         if (!isClient)
             return;
-        
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            Debug.LogError("TOUCH OVER NETWORK");
+            CmdPlaySound("normal sound");
+        }
+
         if (Input.GetKeyDown(KeyCode.Space)) {
             Debug.LogError("SPACE NETWORK");
             CmdPlaySound("normal sound");
