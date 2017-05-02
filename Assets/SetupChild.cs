a377b8cc7df6e4f57cee78745116c0b863083f97
Clement-R <rondeau.clement@gmail.com>
2017-05-02 23:44:42 +0200

    Add all prototypes animations

---
+++ Assets/SetupChild.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupChild : MonoBehaviour {
+
+    public void OnValidate() {
+        MeshFilter mf = GetComponent<MeshFilter>();
+        if(mf.sharedMesh != null) {
+            Mesh meshCopy = Mesh.Instantiate(mf.sharedMesh) as Mesh;
+            gameObject.transform.GetChild(0).GetComponent<MeshFilter>().sharedMesh = meshCopy;
+        } else {
+            Debug.Log("Deb");
+        }
+    }
+}
