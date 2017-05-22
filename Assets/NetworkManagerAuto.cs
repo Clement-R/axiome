8c72131181ff9ce2d24354b189552723a4b1ae2d
Clement-R <rondeau.clement@gmail.com>
2017-05-22 19:49:43 +0200

    WIP : server auto-connection

--- Assets/NetworkManagerAuto.cs
+++ Assets/NetworkManagerAuto.cs
@@ -2,25 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class NetworkManagerAuto : MonoBehaviour {
+    public GameObject debugLog;
+    public GameObject debugLog2;
+
     private NetworkManager manager;
-    private NetworkClient client = null;
 
     void Awake() {
         manager = GetComponent<NetworkManager>();
     }
 
     void Update() {
-        if (Application.platform == RuntimePlatform.WindowsPlayer) {
+        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor) {
             if(!NetworkServer.active) {
                 manager.StartServer();
             }
-        }
+        } else if (Application.platform == RuntimePlatform.Android) {
+            bool noConnection = (manager.client == null || manager.client.connection == null ||
+                                 manager.client.connection.connectionId == -1);
 
-        else if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.WindowsEditor) {
-            if(client == null) {
-                client = manager.StartClient();
+            if (noConnection) {
+                manager.StartClient();
+                debugLog.GetComponent<Text>().text = "Disconnected";
+                debugLog2.GetComponent<Text>().text = manager.networkPort + "";
+            } else {
+                debugLog.GetComponent<Text>().text = "Connected";
             }
         }
     }
