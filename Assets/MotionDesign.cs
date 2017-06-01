45dce682d12da8b02e9c02adfdbecc5d59654539
C.Rondeau <rondeau.clement@gmail.com>
2017-06-01 16:23:29 +0200

    Add new component to display text animation

---
+++ Assets/MotionDesign.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionDesign : MonoBehaviour {
+    public int id;
+
+	void Start () {
+        Object text = Resources.Load("MotionDesign/text (" + id +")", typeof(GameObject));
+        GameObject obj = Instantiate(text, transform) as GameObject;
+        obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, transform.position.z);
+    }
+}
