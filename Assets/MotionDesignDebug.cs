ec724186060d086e595d8ae7a4d7e4dddf3bd8ed
Clement-R <rondeau.clement@gmail.com>
2017-06-01 15:06:14 +0200

    Add new script to load motion design animations at runtime

---
+++ Assets/MotionDesignDebug.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionDesignDebug : MonoBehaviour {
+	void Start () {
+        Object[] texts = Resources.LoadAll("MotionDesign", typeof(GameObject));
+
+        foreach (var text in texts) {
+            GameObject obj = Instantiate(text, transform) as GameObject;
+            obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, transform.position.z);
+        }	
+	}
+}
