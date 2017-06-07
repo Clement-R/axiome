80eb5c5f8190dedc36c17e5f3a51fc3cf476132c
C.Rondeau <rondeau.clement@gmail.com>
2017-06-07 21:39:24 +0200

    Solved out of memory exception caused by huge motion design textures

---
+++ Assets/Scripts/Editor/TextureManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+public class TextureManager : EditorWindow {
+
+    [MenuItem("Window/Texture Manager")]
+    public static void ShowWindow() {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(TextureManager));
+    }
+
+    public void OnGUI() {
+        string[] path = { "Assets/Assets/MotionDesign" };
+        if (GUILayout.Button("Reimport textures")) {
+            string[] motionDesignTextures = AssetDatabase.FindAssets("Comp t:texture2D", path);
+            foreach (var texture in motionDesignTextures) {
+                TextureImporter textureSettings = TextureImporter.GetAtPath(AssetDatabase.GUIDToAssetPath(texture)) as TextureImporter;
+
+                textureSettings.maxTextureSize = 1024;
+                textureSettings.mipmapEnabled = false;
+
+                textureSettings.SaveAndReimport();
+            }
+        }
+    }
+}
+
+#endif
